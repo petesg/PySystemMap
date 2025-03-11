3ca12f85b75d0fbdfe5b21211f04b4be1896cce1
petesg <26416259+petesg@users.noreply.github.com>
2025-03-11 09:17:02 -0500

    database schema mostly planned out

--- src/SystemComponents/ElectronicSystems.py
+++ src/SystemComponents/ElectronicSystems.py
@@ -1,16 +1,54 @@
 import SystemMap
+import sqlite3
+import json
 
-class Pinout(SystemMap.MapObject):
+class PinMap(SystemMap.MapObject):
     pin: str
     net: str
+
+    @classmethod
+    def SetupDbTable(cls, dbConnection: sqlite3.Connection) -> None:
+        cursor = dbConnection.cursor()
+        cursor.execute('''
+                       CREATE TABLE pinouts (
+                       connection INTEGER NOT NULL REFERENCES connections(rowid) ON DELETE CASCADE,
+                       net INTEGER NOT NULL REFERENCES nets(rowid) ON DELETE CASCADE,
+                       pin NOT NULL TEXT,
+                       extraJson TEXT)
+                       ''')
+        dbConnection.commit()
+
 
 class Net(SystemMap.MapObject):
     name: str
 
+    @classmethod
+    def SetupDbTable(cls, dbConnection: sqlite3.Connection) -> None:
+        cursor = dbConnection.cursor()
+        cursor.execute('CREATE TABLE nets (name TEXT NOT NULL UNIQUE, bus INTEGER NOT NULL REFERENCES busses(rowid) ON DELETE CASCADE, extraJson TEXT)')
+        dbConnection.commit()
+
+
 class Bus(SystemMap.MapObject):
     name: str
     signal: str | None
-    nets: list[Net] | None
+    nets: list[Net] = []
+    
+    def StoreInDb(self, dbConnection: sqlite3.Connection) -> None:
+        cursor = dbConnection.cursor()
+        cursor.execute('INSERT INTO busses (name, signal) VALUES (?, ?)', (self.name, self.signal))
+        dbConnection.commit()
+        cursor.execute('SELECT LAST_INSERT_ROWID();')
+        busid = cursor.fetchone()[0]
+        for net in self.nets:
+            cursor.execute('INSERT INTO nets (name, bus, extraJson) VALUES (?, ?, ?)', (net.name, busid, net.extraJson, json.dumps(net.extraJson)))
+
+    @classmethod
+    def SetupDbTable(cls, dbConnection: sqlite3.Connection) -> None:
+        cursor = dbConnection.cursor()
+        cursor.execute('CREATE TABLE busses (name TEXT NOT NULL UNIQUE, signal TEXT, extraJson TEXT)')
+        dbConnection.commit()
+
 
 class Connection(SystemMap.MapObject):
     name: str
@@ -19,10 +57,32 @@ class Connection(SystemMap.MapObject):
     intConnector: bool | None
     connector: str | None
     direction: str | None
-    pinout: list[Pinout]
+    pinout: list[PinMap] = []
 
-class Node(SystemMap.MapObject):
+    @classmethod
+    def SetupDbTable(cls, dbConnection: sqlite3.Connection) -> None:
+        cursor = dbConnection.cursor()
+        cursor.execute('''  CREATE TABLE connections (
+                            name TEXT NOT NULL UNIQUE,
+                            node INTEGER NOT NULL REFERENCES nodes(rowid) ON DELETE CASCADE,
+                            bus INTEGER REFERENCES busses(rowid) ON DELETE CASCADE,
+                            intcable BOOLEAN,
+                            intconn BOOLEAN,
+                            connector TEXT,
+                            direction VARCHAR(2),
+                            extraJson TEXT
+                            )''')
+        dbConnection.commit()
+
+
+class ENode(SystemMap.MapObject):
     name: str
     location: str | None
     connections: list[Connection]
+
+    @classmethod
+    def SetupDbTable(cls, dbConnection: sqlite3.Connection) -> None:
+        cursor = dbConnection.cursor()
+        cursor.execute('CREATE TABLE enodes (name TEXT NOT NULL UNIQUE, location TEXT, extraJson TEXT)')
+        dbConnection.commit()
     
--- src/SystemMap.py
+++ src/SystemMap.py
@@ -4,6 +4,7 @@ import os
 import re
 from _typeshed import SupportsRead
 from typing import Iterable, get_type_hints
+# from SystemComponents.ElectronicSystems import Bus, Connection, Net, Node, Pinout
 
 class SystemMap:
 
@@ -22,7 +23,7 @@ class SystemMap:
                 raise FileExistsError(f'Map "{name}" already exists')
         
         # open the database connection
-        self._db = sqlite3.connect(dbPath)
+        self._ConnectDb(dbPath)
         
         self._dataDir = dataDir
         self._SetupDb()
@@ -31,16 +32,17 @@ class SystemMap:
         if jsonStr:
             self.LoadFromJson(jsonStr, False)
         
-
     def __init__(self, dbDir: str):
         """Opens a sqlite .db file of a system map."""
-
-        self._db = sqlite3.connect(dbDir)
-
+        self._ConnectDb(dbDir)
 
     def __del__(self):
         self._db.close()
 
+    def _ConnectDb(self, dbDir: str):
+        self._db = sqlite3.connect(dbDir)
+        cursor = self._db.cursor()
+        cursor.execute('PRAGMA foreign_keys = ON')
 
     def LoadFromJson(self, jsonStr: str, eraseExisting: bool) -> list[str]:
         output: list[str] = []
@@ -82,9 +84,10 @@ class SystemMap:
                 for net in nets:
                     name = net['name']
                     cursor.execute('INSERT INTO nets (name, bus, extrajson) VALUES (?, ?, ?)', [name, busId, ])
-            
 
-    def _SetupDb(self):
+        return output
+
+    def _SetupDb(self) -> None:
         cursor = self._db.cursor()
 
         cursor.execute('CREATE TABLE nodes (name TEXT NOT NULL UNIQUE, location TEXT, extraJson TEXT)')
@@ -106,46 +109,55 @@ class SystemMap:
         self._db.commit()
         cursor.close()
     
-
-    class MapObject:
-        extraJson: dict[str, any] | None
-        def __init__(self, jsonDict: dict[str, any], recognizedMembers: Iterable['MapObject']):
-            # TODO make sure all recognizedMembers are our children (or is that ok if they're not?)
-            # get list of properties in this object
-            myTypes = get_type_hints(self)
-            # myProps = [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
-            # ^ this doesn't work because dir() doesn't list properties that were just type hinted and not assigned a value
-            myProps = myTypes.keys()
-            
-            # load in all the keys we're getting
-            for key in jsonDict.keys():
-                incProp = re.sub(r'(a-zA-Z)[\s,.]+([a-zA-Z])', lambda m : m.group(0) + m.group(1).upper(), key)
-                incVal = jsonDict[key]
-                # see if the key matches any of our properties
-                if incProp in myProps:
-                    # first check if incoming value matches the expected type
-                    if isinstance(incVal, myTypes[incProp]):
-                        # easy to handle, assign the value to our property
-                        self.__setattr__(incProp, incVal)
-                    # if not, check if our prop is a special type
-                    elif myTypes[incProp] in recognizedMembers:
-                        # instantiate a new object of that type for the member and hand it the json contents we are looking at
-                        self.__setattr__(incProp, globals()[myTypes[incProp]](incVal, recognizedMembers))
-                    # finally, check if it is a list of a special type
-                    elif myTypes[incProp] in [list[t] for t in recognizedMembers]:
-                        self.__setattr__(incProp, [globals()[myTypes[incProp]](v, recognizedMembers) for v in incVal])
-                    # if it's something else, yell about it
-                    else:
-                        raise TypeError(f'Incoming JSON "{key}" must contain "{myTypes[key]}", {type(incVal)} not allowed.')
-                # put any unexpected keys and their contents in the extraJson dict
+class MapObject():
+    extraJson: dict[str, any] = {}
+    def __init__(self, jsonDict: dict[str, any], recognizedMembers: Iterable['MapObject']):
+        '''
+        Base class for members in a system map.  
+        '''
+        # TODO make sure all recognizedMembers are our children (or is that ok if they're not?)
+        # get list of properties in this object
+        myTypes = get_type_hints(self)
+        # myProps = [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]
+        # ^ this doesn't work because dir() doesn't list properties that were just type hinted and not assigned a value
+        myProps = myTypes.keys()
+        
+        # load in all the keys we're getting
+        for key in jsonDict.keys():
+            incProp = re.sub(r'(a-zA-Z)[\s,.]+([a-zA-Z])', lambda m : m.group(0) + m.group(1).upper(), key)
+            incVal = jsonDict[key]
+            # see if the key matches any of our properties
+            if incProp in myProps:
+                # first check if incoming value matches the expected type
+                if isinstance(incVal, myTypes[incProp]):
+                    # easy to handle, assign the value to our property
+                    self.__setattr__(incProp, incVal)
+                # if not, check if our prop is a special type
+                elif myTypes[incProp] in recognizedMembers:
+                    # instantiate a new object of that type for the member and hand it the json contents we are looking at
+                    self.__setattr__(incProp, globals()[myTypes[incProp]](incVal, recognizedMembers))
+                # finally, check if it is a list of a special type
+                elif myTypes[incProp] in [list[t] for t in recognizedMembers]:
+                    self.__setattr__(incProp, [globals()[myTypes[incProp]](v, recognizedMembers) for v in incVal])
+                # if it's something else, yell about it
                 else:
-                    self.extraJson[key] = incVal
+                    raise TypeError(f'Incoming JSON "{key}" must contain "{myTypes[key]}", {type(incVal)} not allowed.')
+            # put any unexpected keys and their contents in the extraJson dict
+            else:
+                self.extraJson[key] = incVal
 
-            # make sure all required keys are supplied
-            for incProp in myProps:
-                missingProps = []
-                if not isinstance(None, myTypes[incProp]) and self.__getattribute__(incProp) is None:
-                    missingProps.append(incProp)
-                if missingProps:
-                    missingStr = ', '.join([f'"{p}"' for p in missingProps])
-                    raise TypeError(f'Incoming JSON missing required key{"s" if len(missingProps) > 1 else ""}: {missingStr}')
+        # make sure all required keys are supplied
+        for incProp in myProps:
+            missingProps = []
+            if not isinstance(None, myTypes[incProp]) and self.__getattribute__(incProp) is None:
+                missingProps.append(incProp)
+            if missingProps:
+                missingStr = ', '.join([f'"{p}"' for p in missingProps])
+                raise TypeError(f'Incoming JSON missing required key{"s" if len(missingProps) > 1 else ""}: {missingStr}')
+
+    def StoreInDb(self, dbConnection: sqlite3.Connection) -> None:
+        raise NotImplementedError(f'{type(self)} has not implemented a direct-to-database storage method.  Either this class is meant to only be loaded by a class containing it as a member, or the `StoreInDb()` method needs to be implemented.')
+
+    @classmethod
+    def SetupDbTable(cls, dbConnection: sqlite3.Connection) -> None:
+        raise NotImplementedError(f'{cls} is missing `SetupDbTable()` implementation.')
