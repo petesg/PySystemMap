70a52db42815e97d017f32d0eac4d7b1314d05fb
petesg <26416259+petesg@users.noreply.github.com>
2025-03-15 11:36:32 -0400

    idiotic code to get example to run without import errors

--- examples/Example.py
+++ examples/Example.py
@@ -1,3 +1,25 @@
+# some BS code to copy ourselves into the root directory to run because Python is stupid and .. imports don't work (on Linux?) somehow
+import re
+import os
+from pathlib import Path
+amStupidCopy = False
+if os.name == 'posix' and not amStupidCopy:
+    with open(__file__) as me:
+        meStr = me.read()
+    meStr = re.sub(r'from\s+\.\.([a-zA-Z._]+)\s+import\s+([a-zA-Z._]+)', lambda m : f'from {m.group(1)} import {m.group(2)}', meStr)
+    meStr = meStr.replace('amStupidCopy = False', 'amStupidCopy = True')
+    bsExcLoc = os.path.join(Path(__file__).parent.parent.absolute(), 'stupidExampleFile.py')
+    with open(bsExcLoc, 'w+') as stupidMe:
+        stupidMe.write(meStr)
+    os.system(f'python3 {bsExcLoc}')
+    os.remove(bsExcLoc)
+    exit()
+
+
+# ACTUAL CODE STARTS HERE
+###########################
+
+
 import json
 
 from ..src import SystemMap
